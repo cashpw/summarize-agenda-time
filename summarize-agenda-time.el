2dcbfb18c8f94c602e97ed8c1f0ba5ecdd8bc646
Cash Weaver <cashbweaver@gmail.com>
2023-11-27 18:36:52 -0800

    feat: Refactor duration calculation (and more)
    - Refactor duration calculation based on text properties
    - Replace "effort" with "duration" where appropriate

--- summarize-agenda-time.el
+++ summarize-agenda-time.el
@@ -23,92 +23,111 @@
 
 (defgroup summarize-agenda-time nil
   "Options related to the clocktable-by-tag dblock."
-  :tag "Org-agenda Summarize Efforts"
+  :tag "Org-agenda summarize agenda time"
   :group 'org)
 
-(defcustom summarize-agenda-time--show-max-effort
+(defcustom summarize-agenda-time--show-max-duration
   t
-  "Show maximum effort in agenda headline if non-nil."
+  "Show maximum duration in agenda headline if non-nil."
   :group 'summarize-agenda-time
   :type 'boolean)
 
-(defcustom summarize-agenda-time--max-effort-minutes
+(defcustom summarize-agenda-time--max-duration-minutes
   (* 8 60)
-  "Maximum effort in minutes.
+  "Maximum duration in minutes.
 
-Displayed in agenda headline when `org-agenda-summarize-effort--show-max-effort' is non-nil."
+Displayed in agenda headline when `org-agenda-summarize-duration--show-max-duration' is non-nil."
   :group 'summarize-agenda-time
   :type 'number)
 
-(defun summarize-agenda-time--get-scheduled-duration-in-minutes (pos)
-  "Return the scheduled duration, in minutes, of the heading at POS, or nil."
-  (let ((timestamp (org-entry-get pos "SCHEDULED")))
-    (when timestamp
-      (let ((timestamp-with-duration-regexp "\\(<.*\\)\\([0-9][0-9]:[0-9][0-9]\\)-\\([0-9][0-9]:[0-9][0-9]\\)\\(.*>\\)"))
-        (when (string-match timestamp-with-duration-regexp
-                            timestamp)
-          (let* ((start-org-time (org-time-string-to-time
-                                  (replace-regexp-in-string timestamp-with-duration-regexp
-                                                            "\\1\\2\\4"
-                                                            timestamp)))
-                 (end-org-time (org-time-string-to-time
-                                (replace-regexp-in-string timestamp-with-duration-regexp
-                                                          "\\1\\3\\4"
-                                                          timestamp))))
-            (/ (org-time-subtract end-org-time
-                                  start-org-time)
-               60)))))))
-
-(defun summarize-agenda-time--summarize-efforts (limit)
-  "Return sum of minutes of effort or scheduled time between point and LIMIT."
-  (let (total)
+(defun summarize-agenda-time--get-total-duration (start end)
+  "Return sum of agenda item durations between START and END points."
+  (let (durations)
     (save-excursion
-      (while (< (point) limit)
-        (let* ((pos (org-get-at-bol 'org-hd-marker))
-               (effort (org-entry-get pos
-                                      "Effort"))
-               (headline-text (org-entry-get pos
-                                             "ITEM"))
-               (scheduled-duration-in-minutes (summarize-agenda-time--get-scheduled-duration-in-minutes pos)))
-          (message "%s %s %s" headline-text effort scheduled-duration-in-minutes)
-          (push (if scheduled-duration-in-minutes
-                    scheduled-duration-in-minutes
-                  effort)
-                total))
+      (goto-char start)
+      (while (< (point) end)
+        (let* ((marker (org-get-at-bol 'org-hd-marker))
+               (effort (org-entry-get marker "Effort"))
+               (headline-text (org-entry-get marker "ITEM"))
+               (duration (get-text-property (point) 'duration)))
+          (push (or duration
+                    effort)
+                durations))
         (forward-line)))
     (org-duration-from-minutes
      (cl-reduce #'+
                 (mapcar #'org-duration-to-minutes
                         (cl-remove-if-not 'identity
-                                          total))))))
+                                          durations))))))
 
-(defun summarize-agenda-time--get-next-date-header-pos (pos)
-  "Return position of the next `org-agenda-date-header' after POS, else nil."
-  (let ((value t))
-    (text-property-any pos
-                       (point-max)
-                       'org-agenda-date-header
-                       value)))
+(defun summarize-agenda-time--get-day-ranges ()
+  "Return list of agenda day start and end positions in current buffer in
+ascending order.
 
-(defun summarize-agenda-time--insert ()
-  "Insert the efforts for each day inside the agenda buffer."
+Ascending order is useful because we can insert text without corrupting our character pointers."
   (save-excursion
-    (let (pos)
-      (while (setq pos (summarize-agenda-time--get-next-date-header-pos (point)))
-        (goto-char pos)
-        (end-of-line)
-        (when-let* ((next-day-pos (summarize-agenda-time--get-next-date-header-pos (point)))
-                    (effort (summarize-agenda-time--summarize-efforts next-day-pos))
-                    (effort-text (if summarize-agenda-time--show-max-effort
-                                     (format " (%s/%s)"
-                                             effort
-                                             (org-duration-from-minutes summarize-agenda-time--max-effort-minutes))
-                                   (format " %s"
-                                           effort))))
-          (insert-and-inherit effort-text))
-        (forward-line)))))
+    (goto-char (point-min))
+    (let ((searching t)
+          (start-pos (text-property-any (point)
+                                        (point-max)
+                                        'org-agenda-date-header
+                                        t))
+          end-pos
+          heading-positions)
+      (while searching
+        (goto-char (text-property-not-all start-pos
+                                      (point-max)
+                                      'org-agenda-date-header
+                                      t))
+        (let ((next-date-header-pos (text-property-any (point)
+                                                       (point-max)
+                                                       'org-agenda-date-header
+                                                       t)))
+          (if next-date-header-pos
+              (setq end-pos next-date-header-pos)
+            (progn
+              (setq
+               end-pos (or (text-property-any (point)
+                                              (point-max)
+                                              'face
+                                              'org-agenda-structure)
+                           (point-max))
+               searching nil))))
+        ;; `push' ensures positions are in ascending order.
+        (push `(:start-pos ,start-pos
+                :end-pos ,end-pos)
+              heading-positions)
+        (setq start-pos end-pos))
+      heading-positions)))
 
-(add-hook 'org-agenda-finalize-hook 'summarize-agenda-time--insert)
+(defun summarize-agenda-time--insert-summary (insert-pos start-pos end-pos)
+  "Insert agenda time summary at INSERT-POS for headings between START-POS and END-POS."
+  (message "insert-summary: %s, %s, %s" insert-pos start-pos end-pos)
+  (when-let* ((total-duration (summarize-agenda-time--get-total-duration start-pos
+                                                                         end-pos))
+              (summary (if summarize-agenda-time--show-max-duration
+                           (let ((max-duration (org-duration-from-minutes summarize-agenda-time--max-duration-minutes)))
+                             (format " (%s/%s)"
+                                     total-duration
+                                     max-duration))
+                         (format " (%s)"
+                                 total-duration))))
+    (save-excursion
+      (goto-char insert-pos)
+      (insert-and-inherit summary))))
+
+(defun summarize-agenda-time--summarize ()
+  "Insert the durations for each day inside the agenda buffer."
+  (save-excursion
+    (cl-dolist (day-range (summarize-agenda-time--get-day-ranges))
+      (summarize-agenda-time--insert-summary (save-excursion
+                                               (goto-char (plist-get day-range :start-pos))
+                                               (end-of-line)
+                                               (point))
+                                             (plist-get day-range :start-pos)
+                                             (plist-get day-range :end-pos)))))
+
+(add-hook 'org-agenda-finalize-hook 'summarize-agenda-time--summarize)
 
 (provide 'summarize-agenda-time)
 ;;; summarize-agenda-time.el ends here
