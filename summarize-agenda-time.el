321f58ddd9c1a6d9a4bb33f9474c0bb60aa265a1
Cash Prokop-Weaver <cash@cashpw.com>
2024-11-21 09:34:50 -0800

    ♻ Format buffer
    `elisp-autofmt-buffer`

--- summarize-agenda-time.el
+++ summarize-agenda-time.el
@@ -28,28 +28,24 @@
   :tag "Org-agenda summarize agenda time"
   :group 'org)
 
-(defcustom summarize-agenda-time--enabled
-  t
+(defcustom summarize-agenda-time--enabled t
   "Enable agenda time summarizing when non-nil."
   :group 'summarize-agenda-time
   :type 'boolean)
 
-(defcustom summarize-agenda-time--show-max-duration
-  t
+(defcustom summarize-agenda-time--show-max-duration t
   "Show maximum duration in agenda headline if non-nil."
   :group 'summarize-agenda-time
   :type 'boolean)
 
-(defcustom summarize-agenda-time--max-duration-minutes
-  (* 8 60)
+(defcustom summarize-agenda-time--max-duration-minutes (* 8 60)
   "Maximum duration in minutes.
 
 Displayed in agenda headline when `org-agenda-summarize-duration--show-max-duration' is non-nil."
   :group 'summarize-agenda-time
   :type 'number)
 
-(defcustom summarize-agenda-time--ignore-entry-fns
-  '()
+(defcustom summarize-agenda-time--ignore-entry-fns '()
   "A list of functions which are run during summarization.
 
 Example:
@@ -65,72 +61,45 @@ Ignore any headline for which at least one of these functions returns non-nil."
 
 (defun summarize-agenda-time-enable ()
   "Enable agenda time summarization."
-  (setq
-   summarize-agenda-time--enabled t))
+  (setq summarize-agenda-time--enabled t))
 
 (defun summarize-agenda-time-disable ()
   "Disable agenda time summarization."
-  (setq
-   summarize-agenda-time--enabled nil))
+  (setq summarize-agenda-time--enabled nil))
 
 (defun summarize-agenda-time--get-property (pom property)
   "Return value of PROPERTY at POM, else nil."
-  (let ((property-list
-         (org-entry-properties
-          pom
-          property)))
+  (let ((property-list (org-entry-properties pom property)))
     (when property-list
-      (cdr
-       (car
-        property-list)))))
+      (cdr (car property-list)))))
 
 (defun summarize-agenda-time--get-total-minutes (start end)
   "Return sum of agenda item durations between START and END points."
   (let (durations)
     (save-excursion
-      (goto-char
-       start)
-      (while (< (point)
-                end)
-        (when (member
-               (org-get-at-bol 'type)
-               '("scheduled"
-                 "past-scheduled"))
-          (let ((marker
-                 (org-get-at-bol
-                  'org-hd-marker)))
+      (goto-char start)
+      (while (< (point) end)
+        (when (member (org-get-at-bol 'type) '("scheduled" "past-scheduled"))
+          (let ((marker (org-get-at-bol 'org-hd-marker)))
             (unless (--any
                      (apply it `(,marker))
                      summarize-agenda-time--ignore-entry-fns)
-              (let ((duration
-                     (get-text-property
-                      (point)
-                      'duration))
+              (let ((duration (get-text-property (point) 'duration))
                     (effort
-                     (summarize-agenda-time--get-property
-                      marker
-                      "Effort")))
-                (push (or duration
-                          effort)
-                      durations)))))
+                     (summarize-agenda-time--get-property marker "Effort")))
+                (push (or duration effort) durations)))))
         (forward-line)))
     (cl-reduce
      #'+
      (mapcar
-      #'org-duration-to-minutes
-      (cl-remove-if-not
-       'identity
-       durations)))))
+      #'org-duration-to-minutes (cl-remove-if-not 'identity durations)))))
 
 (defun summarize-agenda-time--has-agenda-date-header-p ()
   "Return non-nil when the current buffer contains text with the
 `org-agenda-date-header' property."
   (save-excursion
     (goto-char (point-min))
-    (text-property-any (point)
-                       (point-max)
-                       'org-agenda-date-header
-                       t)))
+    (text-property-any (point) (point-max) 'org-agenda-date-header t)))
 
 (defun summarize-agenda-time--get-day-ranges ()
   "Return list of agenda day start and end positions in current buffer in
@@ -140,54 +109,52 @@ Ascending order is useful because we can insert text without corrupting our char
   (save-excursion
     (goto-char (point-min))
     (let ((searching t)
-          (start-pos (text-property-any (point)
-                                        (point-max)
-                                        'org-agenda-date-header
-                                        t))
+          (start-pos
+           (text-property-any (point) (point-max) 'org-agenda-date-header t))
           end-pos
           heading-positions)
       (while searching
-        (goto-char (text-property-not-all start-pos
-                                          (point-max)
-                                          'org-agenda-date-header
-                                          t))
-        (let ((next-date-header-pos (text-property-any (point)
-                                                       (point-max)
-                                                       'org-agenda-date-header
-                                                       t)))
+        (goto-char
+         (text-property-not-all
+          start-pos (point-max) 'org-agenda-date-header t))
+        (let ((next-date-header-pos
+               (text-property-any
+                (point) (point-max) 'org-agenda-date-header t)))
           (if next-date-header-pos
               (setq end-pos next-date-header-pos)
             (progn
               (setq
-               end-pos (or (text-property-any (point)
-                                              (point-max)
-                                              'face
-                                              'org-agenda-structure)
-                           (point-max))
+               end-pos
+               (or (text-property-any
+                    (point) (point-max) 'face 'org-agenda-structure)
+                   (point-max))
                searching nil))))
         ;; `push' ensures positions are in ascending order.
-        (push `(:start-pos ,start-pos
-                :end-pos ,end-pos)
-              heading-positions)
+        (push `(:start-pos ,start-pos :end-pos ,end-pos) heading-positions)
         (setq start-pos end-pos))
       heading-positions)))
 
 (defun summarize-agenda-time--insert-summary (insert-pos start-pos end-pos)
   "Insert agenda time summary at INSERT-POS for headings between START-POS and END-POS."
-  (when-let* ((total-minutes (summarize-agenda-time--get-total-minutes start-pos
-                                                                       end-pos))
+  (when-let* ((total-minutes
+               (summarize-agenda-time--get-total-minutes start-pos end-pos))
               (total-duration (org-duration-from-minutes total-minutes))
-              (summary (if summarize-agenda-time--show-max-duration
-                           (let ((max-duration (org-duration-from-minutes summarize-agenda-time--max-duration-minutes))
-                                 (percentage (round (* 100
-                                                       (/ total-minutes
-                                                          summarize-agenda-time--max-duration-minutes)))))
-                             (format " (%s/%s; %s%%)"
-                                     total-duration
-                                     max-duration
-                                     percentage))
-                         (format " (%s)"
-                                 total-duration))))
+              (summary
+               (if summarize-agenda-time--show-max-duration
+                   (let ((max-duration
+                          (org-duration-from-minutes
+                           summarize-agenda-time--max-duration-minutes))
+                         (percentage
+                          (round
+                           (*
+                            100
+                            (/ total-minutes
+                               summarize-agenda-time--max-duration-minutes)))))
+                     (format " (%s/%s; %s%%)"
+                             total-duration
+                             max-duration
+                             percentage))
+                 (format " (%s)" total-duration))))
     (save-excursion
       (goto-char insert-pos)
       (insert-and-inherit summary))))
@@ -198,12 +165,12 @@ Ascending order is useful because we can insert text without corrupting our char
              (summarize-agenda-time--has-agenda-date-header-p))
     (save-excursion
       (cl-dolist (day-range (summarize-agenda-time--get-day-ranges))
-        (summarize-agenda-time--insert-summary (save-excursion
-                                                 (goto-char (plist-get day-range :start-pos))
-                                                 (end-of-line)
-                                                 (point))
-                                               (plist-get day-range :start-pos)
-                                               (plist-get day-range :end-pos))))))
+        (summarize-agenda-time--insert-summary
+         (save-excursion
+           (goto-char (plist-get day-range :start-pos))
+           (end-of-line)
+           (point))
+         (plist-get day-range :start-pos) (plist-get day-range :end-pos))))))
 
 (add-hook 'org-agenda-finalize-hook 'summarize-agenda-time--summarize)
 
